79073b326f7db028c517793e00abcf86b2be5f48
Yuri Baldan <yuribbp@gmail.com>
2020-10-11 11:16:03 -0300

    Creating project

---
+++ src/test/java/com/stscourse/springbootmongodb/SpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stscourse.springbootmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
